1d81330a070d22272fdbf83fc1f3ba1d83884e26
habibbouchenaki <bouchenaki.habib@yahoo.fr>
2022-05-09 11:41:23 +0200

    added new views

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm, LeadModelForm
 # Create your views here.
 
 def lead_list(request):
@@ -16,3 +17,35 @@ def lead_detail (request, pk) :
         'lead': lead
     }
     return render(request, "leads/lead_detail.html", context)
+
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads/")
+    context = {
+        'form': form
+    }
+    return render(request, "leads/lead_create.html", context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads/")
+    context = {
+        'lead': lead,
+        'form': form
+    }
+    return render(request, "leads/lead_update.html", context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads/")
